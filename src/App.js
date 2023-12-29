2981fe6ebfff5480564d9d84dd189a24380e46ed
Dennis <dennis2441@gmail.com>
2023-12-29 10:52:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,65 @@
-import React, { useRef, useEffect } from "react";
-import {
-  FacebookShareButton,
-  WhatsappShareButton,
-  WhatsappIcon,
-  FacebookIcon,
-} from 'react-share';
+import React, { useState } from "react";
+import { FacebookShareButton, WhatsappShareButton } from 'react-share';
 import "./App.css";
 
 function App() {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const [estadoBluetooth, setEstadoBluetooth] = useState("Desconectado");
 
-  useEffect(() => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      const constraints = { video: true };
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then((stream) => {
-          const video = videoRef.current;
-          video.srcObject = stream;
+  const conectarBluetooth = async () => {
+    try {
+      const response = await fetch('http://tu-servidor-flask/conectar_bluetooth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-          // Esperar a que los metadatos del video se carguen antes de reproducir
-          video.onloadedmetadata = () => {
-            video.play();
-          };
-        })
-        .catch((err) => console.error("Error: " + err));
+      const data = await response.json();
+
+      if (response.ok) {
+        setEstadoBluetooth("Conectado");
+        console.log(data.message);
+      } else {
+        setEstadoBluetooth("Error");
+        console.error(data.error);
+      }
+    } catch (error) {
+      setEstadoBluetooth("Error");
+      console.error('Error al llamar a la API:', error);
     }
-  }, []);
-
-  const Enviarfoto = () =>{
-    
-    console.log("Se envia foto para ver si fuciona fuck");
-  }
-
-  const capturePhoto = () => {
-    const context = canvasRef.current.getContext("2d");
-    context.drawImage(videoRef.current, 0, 0, 640, 480);
-
-    // Convertir la imagen del canvas a formato de imagen (por ejemplo, PNG)
-    const imageDataURL = canvasRef.current.toDataURL("image/png");
-
-    // Crear un elemento <a> para descargar la imagen
-    const downloadLink = document.createElement("a");
-    downloadLink.href = imageDataURL;
-    downloadLink.download = "captura.png"; // Nombre por defecto para la imagen
-
-    // Simular un click para iniciar la descarga
-    downloadLink.click();
   };
-  const url = 'https://i.pinimg.com/564x/f6/12/28/f6122863599199c52911e92700cda8b0.jpg';
-  const message = 'Mensaje opcional';
+
+  const desconectarBluetooth = async () => {
+    try {
+      const response = await fetch('http://tu-servidor-flask/desconectar_bluetooth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setEstadoBluetooth("Desconectado");
+        console.log(data.message);
+      } else {
+        setEstadoBluetooth("Error");
+        console.error(data.error);
+      }
+    } catch (error) {
+      setEstadoBluetooth("Error");
+      console.error('Error al llamar a la API:', error);
+    }
+  };
+
   return (
     <div>
-      <video ref={videoRef} width="640" height="480" autoPlay />
-      <button onClick={capturePhoto}>Capturar Foto</button>
-      
-      <canvas
-        ref={canvasRef}
-        width="640"
-        height="480"
-        style={{ display: "none" }}
-      />
-     <WhatsappShareButton url={url} title={message}>
-      Compartir en WhatsApp
-    </WhatsappShareButton>
+      <h1>Estado Bluetooth: {estadoBluetooth}</h1>
+      <button onClick={conectarBluetooth}>Conectar Bluetooth</button>
+      <button onClick={desconectarBluetooth}>Desconectar Bluetooth</button>
+      {/* Agrega más elementos y funciones según sea necesario */}
     </div>
-
-
   );
 }
 
